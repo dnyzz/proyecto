09fcbec85b71feeb3d688bd07392e8c2f0f7e2bf
dnyzz <cacl01004mgtlrsa0@cbtis217.edu.mx>
2025-12-15 23:41:55 -0600

    pasteleria casi acabada, falta la parte de abajo y conectactar a mysql

--- login/pasteleria.Designer.cs
+++ login/pasteleria.Designer.cs
@@ -36,8 +36,7 @@ namespace proyecto_final
 		/// </summary>
 		private void InitializeComponent()
 		{
-			System.Windows.Forms.ListViewItem listViewItem1 = new System.Windows.Forms.ListViewItem(new string[] {
-									"PRODUCTO"}, -1, System.Drawing.Color.Red, System.Drawing.Color.Empty, null);
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(pasteleria));
 			this.pictureBox1 = new System.Windows.Forms.PictureBox();
 			this.rdbDulce = new System.Windows.Forms.RadioButton();
 			this.rdbSalado = new System.Windows.Forms.RadioButton();
@@ -49,6 +48,7 @@ namespace proyecto_final
 			this.btnEliminar = new System.Windows.Forms.Button();
 			this.btnAgregar = new System.Windows.Forms.Button();
 			this.listView1 = new System.Windows.Forms.ListView();
+			this.chPrecio = new System.Windows.Forms.ColumnHeader();
 			this.lblSubT = new System.Windows.Forms.Label();
 			this.txtSubT = new System.Windows.Forms.TextBox();
 			this.lblDesc = new System.Windows.Forms.Label();
@@ -62,38 +62,46 @@ namespace proyecto_final
 			this.btnGuardar = new System.Windows.Forms.Button();
 			this.btnBorrar = new System.Windows.Forms.Button();
 			this.btnImprimir = new System.Windows.Forms.Button();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.txtPrecio = new System.Windows.Forms.TextBox();
+			this.txtCantidad = new System.Windows.Forms.TextBox();
+			this.txtTotal = new System.Windows.Forms.TextBox();
 			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.panel1.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// pictureBox1
 			// 
 			this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-			this.pictureBox1.Location = new System.Drawing.Point(0, -3);
+			this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+			this.pictureBox1.Location = new System.Drawing.Point(292, -3);
 			this.pictureBox1.Name = "pictureBox1";
-			this.pictureBox1.Size = new System.Drawing.Size(509, 72);
-			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox1.Size = new System.Drawing.Size(217, 72);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
 			this.pictureBox1.TabIndex = 0;
 			this.pictureBox1.TabStop = false;
 			// 
 			// rdbDulce
 			// 
-			this.rdbDulce.Location = new System.Drawing.Point(76, 75);
+			this.rdbDulce.Location = new System.Drawing.Point(12, 15);
 			this.rdbDulce.Name = "rdbDulce";
 			this.rdbDulce.Size = new System.Drawing.Size(104, 24);
 			this.rdbDulce.TabIndex = 1;
 			this.rdbDulce.TabStop = true;
 			this.rdbDulce.Text = "dulce";
 			this.rdbDulce.UseVisualStyleBackColor = true;
+			this.rdbDulce.CheckedChanged += new System.EventHandler(this.RdbDulceCheckedChanged);
 			// 
 			// rdbSalado
 			// 
-			this.rdbSalado.Location = new System.Drawing.Point(262, 75);
+			this.rdbSalado.Location = new System.Drawing.Point(148, 15);
 			this.rdbSalado.Name = "rdbSalado";
 			this.rdbSalado.Size = new System.Drawing.Size(104, 24);
 			this.rdbSalado.TabIndex = 2;
 			this.rdbSalado.TabStop = true;
 			this.rdbSalado.Text = "Salado";
 			this.rdbSalado.UseVisualStyleBackColor = true;
+			this.rdbSalado.CheckedChanged += new System.EventHandler(this.RdbSaladoCheckedChanged);
 			// 
 			// lblSelectProd
 			// 
@@ -107,7 +115,7 @@ namespace proyecto_final
 			// 
 			this.cmbSelectProd.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
 			this.cmbSelectProd.FormattingEnabled = true;
-			this.cmbSelectProd.Location = new System.Drawing.Point(131, 105);
+			this.cmbSelectProd.Location = new System.Drawing.Point(131, 99);
 			this.cmbSelectProd.Name = "cmbSelectProd";
 			this.cmbSelectProd.Size = new System.Drawing.Size(121, 21);
 			this.cmbSelectProd.TabIndex = 4;
@@ -141,7 +149,7 @@ namespace proyecto_final
 			// 
 			this.btnEliminar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
 			this.btnEliminar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-			this.btnEliminar.Location = new System.Drawing.Point(372, 134);
+			this.btnEliminar.Location = new System.Drawing.Point(372, 128);
 			this.btnEliminar.Name = "btnEliminar";
 			this.btnEliminar.Size = new System.Drawing.Size(120, 23);
 			this.btnEliminar.TabIndex = 8;
@@ -153,23 +161,29 @@ namespace proyecto_final
 			this.btnAgregar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
 			this.btnAgregar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
 			this.btnAgregar.ForeColor = System.Drawing.Color.Black;
-			this.btnAgregar.Location = new System.Drawing.Point(372, 97);
+			this.btnAgregar.Location = new System.Drawing.Point(372, 86);
 			this.btnAgregar.Name = "btnAgregar";
 			this.btnAgregar.Size = new System.Drawing.Size(120, 23);
 			this.btnAgregar.TabIndex = 9;
 			this.btnAgregar.Text = "Agregar producto";
 			this.btnAgregar.UseVisualStyleBackColor = false;
+			this.btnAgregar.Click += new System.EventHandler(this.BtnAgregarClick);
 			// 
 			// listView1
 			// 
-			this.listView1.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
-									listViewItem1});
+			this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+									this.chPrecio});
 			this.listView1.Location = new System.Drawing.Point(0, 157);
 			this.listView1.Name = "listView1";
 			this.listView1.Size = new System.Drawing.Size(509, 97);
 			this.listView1.TabIndex = 10;
 			this.listView1.UseCompatibleStateImageBehavior = false;
-			this.listView1.View = System.Windows.Forms.View.Tile;
+			this.listView1.View = System.Windows.Forms.View.Details;
+			listView1.Columns.Clear(); 
+			listView1.Columns.Add("Producto", 150);
+			listView1.Columns.Add("Precio", 70);
+			listView1.Columns.Add("Cantidad", 70);
+			listView1.Columns.Add("Total", 80);
 			// 
 			// lblSubT
 			// 
@@ -279,6 +293,36 @@ namespace proyecto_final
 			this.btnImprimir.Text = "Imprimir";
 			this.btnImprimir.UseVisualStyleBackColor = false;
 			// 
+			// panel1
+			// 
+			this.panel1.Controls.Add(this.rdbDulce);
+			this.panel1.Controls.Add(this.rdbSalado);
+			this.panel1.Location = new System.Drawing.Point(0, -3);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(286, 51);
+			this.panel1.TabIndex = 24;
+			// 
+			// txtPrecio
+			// 
+			this.txtPrecio.Location = new System.Drawing.Point(56, 131);
+			this.txtPrecio.Name = "txtPrecio";
+			this.txtPrecio.Size = new System.Drawing.Size(69, 20);
+			this.txtPrecio.TabIndex = 25;
+			// 
+			// txtCantidad
+			// 
+			this.txtCantidad.Location = new System.Drawing.Point(185, 130);
+			this.txtCantidad.Name = "txtCantidad";
+			this.txtCantidad.Size = new System.Drawing.Size(61, 20);
+			this.txtCantidad.TabIndex = 26;
+			// 
+			// txtTotal
+			// 
+			this.txtTotal.Location = new System.Drawing.Point(284, 131);
+			this.txtTotal.Name = "txtTotal";
+			this.txtTotal.Size = new System.Drawing.Size(68, 20);
+			this.txtTotal.TabIndex = 27;
+			// 
 			// pasteleria
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -288,6 +332,10 @@ namespace proyecto_final
 			this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
 			this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
 			this.ClientSize = new System.Drawing.Size(507, 375);
+			this.Controls.Add(this.txtTotal);
+			this.Controls.Add(this.txtCantidad);
+			this.Controls.Add(this.txtPrecio);
+			this.Controls.Add(this.panel1);
 			this.Controls.Add(this.btnImprimir);
 			this.Controls.Add(this.btnBorrar);
 			this.Controls.Add(this.btnGuardar);
@@ -309,15 +357,19 @@ namespace proyecto_final
 			this.Controls.Add(this.lblPrecio);
 			this.Controls.Add(this.cmbSelectProd);
 			this.Controls.Add(this.lblSelectProd);
-			this.Controls.Add(this.rdbSalado);
-			this.Controls.Add(this.rdbDulce);
 			this.Controls.Add(this.pictureBox1);
 			this.Name = "pasteleria";
 			this.Text = "POSTRES (PASTELERIA)";
 			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.panel1.ResumeLayout(false);
 			this.ResumeLayout(false);
 			this.PerformLayout();
 		}
+		private System.Windows.Forms.TextBox txtTotal;
+		private System.Windows.Forms.TextBox txtCantidad;
+		private System.Windows.Forms.TextBox txtPrecio;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.ColumnHeader chPrecio;
 		private System.Windows.Forms.Button btnImprimir;
 		private System.Windows.Forms.Button btnBorrar;
 		private System.Windows.Forms.Button btnGuardar;
--- login/pasteleria.cs
+++ login/pasteleria.cs
@@ -20,6 +20,11 @@ namespace proyecto_final
 	/// </summary>
 	public partial class pasteleria : Form
 	{
+		string[] Dulces = { "Pastel", "Brownie", "Donas", "Flan", "Helado", "Pay de Queso" };
+		string[] Salados = { "Crepas", "Pizza", "Agua Fresca" };
+
+
+
 		public pasteleria()
 		{
 			//
@@ -36,6 +41,56 @@ namespace proyecto_final
 		{
 	
     
+		}
+		
+		void RdbDulceCheckedChanged(object sender, EventArgs e)
+		{
+			cmbSelectProd.Items.Clear();
+           cmbSelectProd.Items.AddRange(Dulces);
+            cmbSelectProd.SelectedIndex = 0;
+		}
+		
+		void RdbSaladoCheckedChanged(object sender, EventArgs e)
+		{
+			if (rdbSalado.Checked)
+    {
+        cmbSelectProd.Items.Clear(); 
+        cmbSelectProd.Items.AddRange(Salados); 
+     	cmbSelectProd.SelectedIndex = 0; 
+    }
+
+		}
+		
+		void BtnAgregarClick(object sender, EventArgs e)
+		{
+
+    string producto = cmbSelectProd.SelectedItem.ToString();
+    string precioTexto = txtPrecio.Text;
+    string cantidadTexto = txtCantidad.Text;
+    decimal precio;
+    int cantidad;
+    bool precioValido = decimal.TryParse(precioTexto, out precio);
+    bool cantidadValida = int.TryParse(cantidadTexto, out cantidad);
+
+    if (!precioValido || !cantidadValida)
+    {
+        MessageBox.Show("Precio o cantidad inválidos.");
+        return;
+    }
+
+    decimal total = precio * cantidad;
+
+    ListViewItem item = new ListViewItem(producto);        
+    item.SubItems.Add(precio.ToString("0.00"));           
+    item.SubItems.Add(cantidad.ToString());                 
+    item.SubItems.Add(total.ToString("0.00"));            
+    listView1.Items.Add(item);
+    txtTotal.Text = total.ToString("0.00");
+    txtPrecio.Clear();
+    txtCantidad.Clear();
+    cmbSelectProd.SelectedIndex = -1;
+
+
 		}
 	}
 }
